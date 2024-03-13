b102e04b8a946932a1843b36652e7ed1c00e5ddd
Ahmad <ahmadsattar.7867@gmail.com>
2024-03-14 02:21:59 +0500

    pp

---
+++ Assignment # 4/task 2/script.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+    const cursors = $("[data-cursor]");
+    const hoveredElements = $("button, a");
+
+    $(window).on("mousemove", function(event) {
+        const posX = event.clientX;
+        const posY = event.clientY;
+
+        /** cursor dot position */
+        cursors.eq(0).css({
+            left: posX + "px",
+            top: posY + "px"
+        });
+
+        /** cursor outline position */
+        setTimeout(function() {
+            cursors.eq(1).css({
+                left: posX + "px",
+                top: posY + "px"
+            });
+        }, 80);
+    });
+
+    const cursorOutline = $(".cursor-outline");
+    const navbarListItems = $(".navbarr ul li");
+
+    navbarListItems.on("mouseover", function() {
+        cursorOutline.css({
+            transition: "width 0.7s ease, height 0.7s ease",
+            width: "80px",
+            height: "80px"
+        });
+        $(this).css({
+            color: "white",
+            transition: "font-size 0.7s ease",
+            fontSize: "40px"
+        });
+    });
+
+    navbarListItems.on("mouseout", function() {
+        cursorOutline.css({
+            transition: "width 0.7s ease, height 0.7s ease",
+            width: "40px",
+            height: "40px",
+            backgroundColor: "transparent"
+        });
+        $(this).css({
+            color: "",
+            backgroundColor: "",
+            transition: "font-size 0.7s ease",
+            fontSize: "25px",
+            border: "none"
+        });
+    });
+
+
+
+    const navbtn = $(".navbtnn");
+    navbtn.on("mouseover", function() {
+        cursorOutline.css({
+            transition: "width 0.7s ease, height 0.7s ease",
+            color: "white"
+        });
+    
+        $(this).css({
+            color: "white", // Change text color to white
+            transition: "color 0.5s ease" // Apply transition to color change
+        });
+    }).on("mouseout", function() {
+        cursorOutline.css({
+            color: "gray" // Revert cursor color to gray
+        });
+    
+        $(this).css({
+            color: "gray", // Revert text color to gray
+            transition: "color 0.5s ease" // Apply transition to color change
+        });
+    });
+
+
+  
+    
+});
